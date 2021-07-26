3b1add8c1aaa8af55428251f151ec04b5a46938b
seyedalian <seyedalian@outlook.com>
2021-07-26 13:54:19 +0430

    responding to home page request

--- router.js
+++ router.js
@@ -1,9 +1,12 @@
-function route(handle, pathname) {
+function route(handle, pathname, response) {
     console.log("Routing a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
-        handle[pathname]();
+        handle[pathname](response);
     } else {
         console.log("No handler for " + pathname)
+        response.writeHead(404, { "Content-Type": "text/plain" });
+        response.write("Error 404 : page not found!");
+        response.end();
     }
 };
 
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var http = require('http');
 var url = require('url');
 
-function startserver(routeMessage, handle) {
+function startserver(route, handle) {
     http.createServer(function(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request received on pathname = " + pathname)
-        route(handle, pathname)
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.write("Hello Mr.Seyedalian\nRandom:" + Math.round(Math.random() * 10));
-        response.end();
+        route(handle, pathname, response)
     }).listen(8888);
 
     console.log("Server Started on localhost port 8888");
